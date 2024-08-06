cd863527b33a844bee225051793a52feee4cb4c8
vivinkv6 <vivinkv0@gmail.com>
2024-08-06 11:01:10 +0530

    update skeleton

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -38,43 +38,44 @@ export default function SkeletonCard() {
 
 export function BlogDetailSkeleton() {
   return (
-    <div className="flex flex-wrap justify-evenly animate-pulse border border-gray-700 rounded-lg p-6">
-      <div className="w-full">
-        <div className="flex max-lg:flex-wrap gap-x-10 justify-between">
-          <div className="max-lg:w-full bg-gray-700 h-96 max-sm:w-11/12 max-sm:rounded-lg max-sm:h-52 w-8/12 rounded-xl animate-pulse"></div>
-          <div className="w-full lg:w-4/12">
-            <div className="h-8 bg-gray-700 rounded max-sm:w-72 w-full my-5 animate-pulse"></div>
-            <div className="flex flex-row flex-wrap gap-x-5 my-5">
-              {[...Array(3)].map((_, index) => (
-                <span
-                  key={index}
-                  className="h-7 w-20 rounded-md animate-pulse"
-                  style={{
-                    backgroundColor: "#656C7D",
-                  }}
-                ></span>
-              ))}
-            </div>
-            <div className="my-5 flex items-center gap-3 max-sm:hidden">
-              <div className="w-12 h-12 bg-gray-700 rounded-full animate-pulse"></div>
-              <div className="h-4 w-32 bg-gray-700 rounded animate-pulse"></div>
-            </div>
-            <div className="space-y-2 my-10">
-              {[...Array(5)].map((_, index) => (
-                <div
-                  key={index}
-                  className="h-4 bg-gray-700 rounded max-sm:w-96 w-full animate-pulse"
-                ></div>
-              ))}
-            </div>
+    <div className="flex flex-wrap justify-evenly animate-pulse border border-gray-700 rounded-lg p-6 max-sm:p-4">
+    <div className="w-full">
+      <div className="flex flex-wrap lg:flex-nowrap gap-x-10 justify-between">
+        <div className="w-full lg:w-8/12 h-96 max-sm:w-full max-sm:rounded-lg max-sm:h-52 rounded-xl bg-gray-700 animate-pulse"></div>
+        <div className="w-full lg:w-4/12 mt-5 lg:mt-0">
+          <div className="h-8 bg-gray-700 rounded w-full my-5 animate-pulse"></div>
+          <div className="flex flex-row flex-wrap">
+            {[...Array(3)].map((_, index) => (
+              <span
+                key={index}
+                className="m-1 h-7 w-20 rounded-md animate-pulse"
+                style={{
+                  backgroundColor: "#656C7D",
+                }}
+              ></span>
+            ))}
+          </div>
+          <div className="my-5 flex items-center gap-3 max-sm:hidden">
+            <div className="w-12 h-12 bg-gray-700 rounded-full animate-pulse"></div>
+            <div className="h-4 w-32 bg-gray-700 rounded animate-pulse"></div>
+          </div>
+          <div className="space-y-2 my-10">
+            {[...Array(5)].map((_, index) => (
+              <div
+                key={index}
+                className="h-3 bg-gray-700 rounded w-full  animate-pulse"
+              ></div>
+            ))}
           </div>
         </div>
-        <div className="mt-10">
-          {[...Array(15)].map((_, index) => (
-            <div key={index} className="h-4 bg-gray-700 rounded w-full my-2  animate-pulse"></div>
-          ))}
-        </div>
+      </div>
+      <div className="mt-10 space-y-2">
+        {[...Array(15)].map((_, index) => (
+          <div key={index} className="h-2 bg-gray-700 rounded w-full animate-pulse"></div>
+        ))}
       </div>
     </div>
+  </div>
+  
   );
 }
